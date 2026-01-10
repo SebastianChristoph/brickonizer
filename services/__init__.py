dc6933bec09aff5a17f84bf472ba50431164e26e
SebastianChristoph <sebastianchristoph2@gmail.com>
2026-01-10 13:34:14 +0100

    no ocr

--- services/__init__.py
+++ services/__init__.py
@@ -3,11 +3,9 @@ Services package initialization.
 """
 from .brickognize_api import BrickognizeAPI, get_api_instance
 from .image_processor import ImageProcessor
-from .ocr_service import OCRService
 
 __all__ = [
     'BrickognizeAPI',
     'get_api_instance',
-    'ImageProcessor',
-    'OCRService'
+    'ImageProcessor'
 ]
